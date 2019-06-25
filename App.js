bebddbc475f7d953ab547bae3797d330900d806e
DanielWinz75 <netzcourage@mailbox.org>
2019-06-25 12:49:12 +0200

    snack

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// import reducer from './reducers'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Opening up App.js to start working on your app!</Text>
     </View>
   );
 }
